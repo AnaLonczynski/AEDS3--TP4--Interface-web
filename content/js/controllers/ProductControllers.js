5a44d6d33b5ebe948350d144928dad9e66fa0640
Letícia Azevedo <69063558+leh180@users.noreply.github.com>
2025-11-17 17:04:20 -0300

    Delete content directory

--- content/js/controllers/ProductControllers.js
+++
@@ -1,3 +0,0 @@
-import { Produto } from "../models/Produto.js";
-import { ProdutoService } from "../services/ProdutoService.js";
-
--- content/js/services/ProductServices.js
+++
@@ -1,8 +0,0 @@
-import { Produto } from "../models/Produto.js";
-import { IndiceGtin } from "./IndiceGtin.js";
-
-class CRUDProduto{
-
-    CRUDProduto(){}
-
-}
